2b9ae66962b17a611c32e45cfe2bee9d2ad11eb5
Kevin Chen <kcchen33@gmail.com>
2022-01-26 21:31:55 +0100

    update favicon

--- config/config_prod.js
+++ config/config_prod.js
@@ -6,8 +6,4 @@ module.exports = {
   rds_port: process.env.RDS_PORT,
   rds_db: process.env.RDS_DB,
   domain: process.env.DOMAIN,
-  infura_url: process.env.INFURA_URL,
-  accuweather_api_key: process.env.ACCUWEATHER_API_KEY,
-  commission_bp: process.env.COMISSION_PERCENTAGE,
-  comission_account: process.env.COMISSION_ACCOUNT
 };
